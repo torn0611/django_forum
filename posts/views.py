b725c161e69211ca2856d7deda7b00d34a2a7d05
Clay <torn0611@gmail.com>
2022-03-07 12:51:42 -0500

    update

---
+++ posts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+
+
+def index(request):
+    # If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        #If the form is valid
+        if form.is_valid():
+            #Yes, Save
+            form.save()
+             
+            #Redirect to Home
+            return HttpResponseRedirect('/')
+       
+        else:
+            #No, Show Error
+            return HttpResponseRedirect(form.erros.as_json())
+
+    # Get all posts, limit = 20
+    posts = Post.objects.all()[:20]
+    
+    # Show
+    return render(request, 'posts.html',
+    {'posts' : posts})
+
+def delete(request, post_id):
+     post = Post.objects.get(id = post_id)
+     post.delete()
+     return HttpResponseRedirect('/')
+
